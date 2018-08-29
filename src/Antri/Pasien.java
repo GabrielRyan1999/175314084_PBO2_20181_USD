2961a76c53650369d33f8eb5c982e51ea8768a13
Ryan <Ryan@DESKTOP-91QBCFL>
2018-08-29 19:12:08 +0700

    0.2

--- src/Antri/Pasien.java
+++ src/Antri/Pasien.java
@@ -13,95 +13,109 @@ import java.util.Date;
  * @author admin
  */
 public class Pasien {
-    
-    private String nama;
-    private Date tgllahir;
+
+    private String namaPasien;
+
     private String alamat;
     private String tempatLahir;
     private String noRekamMedis;
-    private int tanggalLahir;
-    private int bulanLahir;
-    private int TahunLahir;
-    
+    private int tanggalLahirPasien;
+    private int bulanLahirPasien;
+    private int tahunLahirPasien;
 
-    public static void main(String[] args) {
-        
-    }
-    
     public Pasien(String nama) {
-        this.nama = nama;
-        
+        this.namaPasien = nama;
+
     }
-    
-    public String getNama() {
-        return nama;
+
+    public String getNamaPasien() {
+        return namaPasien;
     }
-    
-    public void setNama(String nama) {
-        this.nama = nama;
+
+    public void setNamaPasien(String namaPasien) {
+        this.namaPasien = namaPasien;
     }
-    
-    public Date getTgllahir() {
-        return tgllahir;
-    }
-    
-    public void setTgllahir(Date tgllahir) {
-        this.tgllahir = tgllahir;
-    }
-    
-    public void setTglLahir(int year, int month, int date) {
-        tgllahir = new Date(year - 1900, month - 1, date);
-    }
-    
+
     public String getAlamat() {
         return alamat;
     }
-    
+
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
-    
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
     public String getNoRekamMedis() {
         return noRekamMedis;
     }
-    
-    public void setNoRekamMedis(String noRekamMedis) {
-        this.noRekamMedis = noRekamMedis;
+
+    public void setNoRekamMedis(String noRekamMedis) throws Exception {
+        if (noRekamMedis.length() >= 6) {
+            this.noRekamMedis = noRekamMedis;
+        } else {
+            throw new Exception("nomor rekam medis salah");
+        }
+
     }
 
-    public int getTanggalLahir() {
-        return tanggalLahir;
+    public int getTanggalLahirPasien() {
+        return tanggalLahirPasien;
     }
 
-    public void setTanggalLahir(int tanggalLahir) {
-        this.tanggalLahir = tanggalLahir;
+    public void setTanggalLahirPasien(int tanggalLahirPasien) throws Exception {
+        if (tanggalLahirPasien > 0) {
+            if (tanggalLahirPasien <= 31) {
+                this.tanggalLahirPasien = tanggalLahirPasien;
+            } else {
+                throw new Exception("tanggal lahir lebih dari 31");
+            }
+        } else {
+            throw new Exception("tanggal lahir kurang dari 1");
+        }
     }
 
-    public int getBulanLahir() {
-        return bulanLahir;
+    public int getBulanLahirPasien() {
+        return bulanLahirPasien;
     }
 
-    public void setBulanLahir(int bulanLahir) {
-        this.bulanLahir = bulanLahir;
+    public void setBulanLahirPasien(int bulanLahirPasien) throws Exception {
+        if (bulanLahirPasien > 0) {
+            if (bulanLahirPasien <= 12) {
+                this.bulanLahirPasien = bulanLahirPasien;
+            } else {
+                throw new Exception("bulan lahir lebih dari 12");
+            }
+        } else {
+            throw new Exception("bulan lahir kurang dari 0");
+        }
     }
 
-    public int getTahunLahir() {
-        return TahunLahir;
+    public int getTahunLahirPasien() {
+        return tahunLahirPasien;
     }
 
-    public void setTahunLahir(int TahunLahir) {
-        this.TahunLahir = TahunLahir;
+    public void setTahunLahirPasien(int tahunLahirPasien) throws Exception {
+        if (tahunLahirPasien > 0) {
+
+            this.tahunLahirPasien = tahunLahirPasien;
+        } else {
+            throw new Exception("tahun lahir kurang dari 1");
+        }
+
     }
-    
-    
-    public int getUsia() {
-        Date today = new Date();
-        return today.getYear() - tgllahir.getYear();
-    }
-    
-    public void AturNomorRekamMedis() {
+
+    public String AturNomorRekamMedis() {
+        String nomorRekamMedis;
         Date tgl = new Date();
         SimpleDateFormat ft = new SimpleDateFormat("yyyMMdd");
-        System.out.println(ft.format(tgl) + nama.substring(0, 3));
+        nomorRekamMedis = ft.format(tgl) + namaPasien.substring(0, 3);
+        return nomorRekamMedis;
     }
 }
