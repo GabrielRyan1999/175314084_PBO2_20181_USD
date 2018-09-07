ae6871a883e3a3ad55865fb4aa52b74d9b2c626c
jarkom <jarkom@jarkom-C00>
2018-09-07 12:49:56 +0700

    0.4

--- src/View/DaftarAntrianDialog.java
+++ src/View/DaftarAntrianDialog.java
@@ -15,7 +15,7 @@ import javax.swing.JTextField;
  * @author jarkom
  */
 public class DaftarAntrianDialog extends JDialog {
-    
+
     private JLabel labelNoRM;
     private JLabel labelNama;
     private JLabel labelAlamat;
@@ -23,41 +23,40 @@ public class DaftarAntrianDialog extends JDialog {
     private JTextField textFieldNama;
     private JTextField textFieldAlamat;
     private JButton buttonSave;
-    
-   
-/** membuat constuctor kosong dengan nama DaftarAntrianDialog()
- * 
- */
+
+    /**
+     * membuat constuctor kosong dengan nama DaftarAntrianDialog()
+     *
+     */
     public DaftarAntrianDialog() {
-        /** mengisi title dengan "Form Daftar Antrian"
-         * 
-         */
+        // mengisi title dengan "Form Daftar Antrian"
+
         this.setTitle("Form Daftar Antrian");
-        /** menjalankan method init()
-         * 
-         */
+        //menjalankan method init()
+
         init();
     }
-    
-    public void init(){
+
+    public void init() {
         this.setLayout(null);
         labelNoRM = new JLabel("NoRM");
+        //mengatur ukuran labelNoRM dengan ukuran 10,10,70,25
         labelNoRM.setBounds(10, 10, 70, 25);
         labelNama = new JLabel("Nama");
         labelNama.setBounds(10, 50, 70, 25);
         labelAlamat = new JLabel("Alamat");
         labelAlamat.setBounds(10, 90, 70, 25);
-        
+
         textFieldNoRM = new JTextField();
         textFieldNoRM.setBounds(90, 10, 170, 25);
         textFieldNama = new JTextField();
         textFieldNama.setBounds(90, 50, 170, 25);
         textFieldAlamat = new JTextField();
         textFieldAlamat.setBounds(90, 90, 170, 25);
-        
+
         buttonSave = new JButton("Save");
         buttonSave.setBounds(110, 140, 70, 25);
-        
+
         this.add(labelNoRM);
         this.add(labelNama);
         this.add(labelAlamat);
@@ -65,6 +64,6 @@ public class DaftarAntrianDialog extends JDialog {
         this.add(textFieldNama);
         this.add(textFieldAlamat);
         this.add(buttonSave);
-       
+
     }
 }
